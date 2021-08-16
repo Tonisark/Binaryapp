0c892c8d248a4ef8ed43d5224b59722242aa248c
Toni2stark <sogoaheadistill@gmail.com>
2021-08-16 10:38:49 +0430

    new

---
+++ app/src/main/java/com/example/binary/Binary.java
@@ -0,0 +1,220 @@
+package com.example.binary;
+
+public class Binary {
+    int   A , D , G , J , M , P;
+    int   B , E , H , K , N , Q;
+    int   C , F , I , L , O , R;
+    int   S , V , Y;
+    int   T , W , Z;
+    int   U , X;
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int a) {
+        A = a;
+    }
+
+    public int getD() {
+        return D;
+    }
+
+    public void setD(int d) {
+        D = d;
+    }
+
+    public int getG() {
+        return G;
+    }
+
+    public void setG(int g) {
+        G = g;
+    }
+
+    public int getJ() {
+        return J;
+    }
+
+    public void setJ(int j) {
+        J = j;
+    }
+
+    public int getM() {
+        return M;
+    }
+
+    public void setM(int m) {
+        M = m;
+    }
+
+    public int getP() {
+        return P;
+    }
+
+    public void setP(int p) {
+        P = p;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public void setB(int b) {
+        B = b;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    public void setE(int e) {
+        E = e;
+    }
+
+    public int getH() {
+        return H;
+    }
+
+    public void setH(int h) {
+        H = h;
+    }
+
+    public int getK() {
+        return K;
+    }
+
+    public void setK(int k) {
+        K = k;
+    }
+
+    public int getN() {
+        return N;
+    }
+
+    public void setN(int n) {
+        N = n;
+    }
+
+    public int getQ() {
+        return Q;
+    }
+
+    public void setQ(int q) {
+        Q = q;
+    }
+
+    public int getC() {
+        return C;
+    }
+
+    public void setC(int c) {
+        C = c;
+    }
+
+    public int getF() {
+        return F;
+    }
+
+    public void setF(int f) {
+        F = f;
+    }
+
+    public int getI() {
+        return I;
+    }
+
+    public void setI(int i) {
+        I = i;
+    }
+
+    public int getL() {
+        return L;
+    }
+
+    public void setL(int l) {
+        L = l;
+    }
+
+    public int getO() {
+        return O;
+    }
+
+    public void setO(int o) {
+        O = o;
+    }
+
+    public int getR() {
+        return R;
+    }
+
+    public void setR(int r) {
+        R = r;
+    }
+
+    public int getS() {
+        return S;
+    }
+
+    public void setS(int s) {
+        S = s;
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public void setV(int v) {
+        V = v;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public int getT() {
+        return T;
+    }
+
+    public void setT(int t) {
+        T = t;
+    }
+
+    public int getW() {
+        return W;
+    }
+
+    public void setW(int w) {
+        W = w;
+    }
+
+    public int getZ() {
+        return Z;
+    }
+
+    public void setZ(int z) {
+        Z = z;
+    }
+
+    public int getU() {
+        return U;
+    }
+
+    public void setU(int u) {
+        U = u;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/binary/MainActivity.java
@@ -0,0 +1,171 @@
+package com.example.binary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView textView;
+    Button Convert_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Please Write CAPS", Toast.LENGTH_LONG).show();
+        textView = findViewById(R.id.showBinary);
+        Convert_btn = findViewById(R.id.Convert_btn);
+        Binary Binary = new Binary();
+
+        Convert_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText = findViewById(R.id.GetBinary);
+                if (editText.getText().toString().equals("A")) {
+                    //Binary A
+                    Binary.setA(1000001);
+                    textView.setText("--> " + Binary.getA());
+                }
+                if (editText.getText().toString().equals("B")) {
+                    //Binary B
+                    Binary.setB(1000010);
+                    textView.setText("--> " + Binary.getB());
+                }
+                if (editText.getText().toString().equals("C")) {
+                    //Binary C
+                    Binary.setC(1000011);
+                    textView.setText("--> " + Binary.getC());
+
+                }
+                if (editText.getText().toString().equals("D")) {
+                    //Binary D
+                    Binary.setD(1000100);
+                    textView.setText("--> " + Binary.getD());
+
+                }
+                if (editText.getText().toString().equals("E")) {
+                    //Binary E
+                    Binary.setE(1000101);
+                    textView.setText("--> " + Binary.getE());
+                }
+
+                if (editText.getText().toString().equals("F")) {
+                    //Binary F
+                    Binary.setF(1000110);
+                    textView.setText("--> " + Binary.getF());
+                }
+
+                if (editText.getText().toString().equals("G")) {
+                    //Binary G
+                    Binary.setG(1000111);
+                    textView.setText("--> " + Binary.getG());
+                }
+
+                if (editText.getText().toString().equals("H")) {
+                    //Binary H
+                    Binary.setH(1001000);
+                    textView.setText("--> " + Binary.getH());
+                }
+                if (editText.getText().toString().equals("I")) {
+                    //Binary I
+                    Binary.setI(1001001);
+                    textView.setText("--> " + Binary.getI());
+                }
+                if (editText.getText().toString().equals("J")) {
+                    //Binary J
+                    Binary.setJ(1001010);
+                    textView.setText("--> " + Binary.getJ());
+                }
+                if (editText.getText().toString().equals("K")) {
+                    //Binary K
+                    Binary.setK(1001011);
+                    textView.setText("--> " + Binary.getK());
+                }
+                if (editText.getText().toString().equals("L")) {
+                    //Binary L
+                    Binary.setL(1001100);
+                    textView.setText("--> " + Binary.getL());
+                }
+                if (editText.getText().toString().equals("M")) {
+                    //Binary M
+                    Binary.setM(1001101);
+                    textView.setText("--> " + Binary.getM());
+                }
+                if (editText.getText().toString().equals("N")) {
+                    //Binary N
+                    Binary.setN(1001110);
+                    textView.setText("--> " + Binary.getN());
+                }
+                if (editText.getText().toString().equals("O")) {
+                    //Binary O
+                    Binary.setO(1001111);
+                    textView.setText("--> " + Binary.getO());
+                }
+                if (editText.getText().toString().equals("P")) {
+                    //Binary P
+                    Binary.setP(1010000);
+                    textView.setText("--> " + Binary.getP());
+                }
+                if (editText.getText().toString().equals("Q")) {
+                    //Binary Q
+                    Binary.setQ(1010001);
+                    textView.setText("--> " + Binary.getQ());
+                }
+                if (editText.getText().toString().equals("R")) {
+                    //Binary R
+                    Binary.setR(1010010);
+                    textView.setText("--> " + Binary.getR());
+                }
+                if (editText.getText().toString().equals("S")) {
+                    //Binary S
+                    Binary.setS(1010011);
+                    textView.setText("--> " + Binary.getS());
+                }
+                if (editText.getText().toString().equals("T")) {
+                    //Binary T
+                    Binary.setT(1010100);
+                    textView.setText("--> " + Binary.getT());
+                }
+                if (editText.getText().toString().equals("U")) {
+                    //Binary U
+                    Binary.setU(1010101);
+                    textView.setText("--> " + Binary.getU());
+                }
+                if (editText.getText().toString().equals("V")) {
+                    //Binary V
+                    Binary.setV(1010110);
+                    textView.setText("--> " + Binary.getV());
+                }
+                if (editText.getText().toString().equals("W")) {
+                    //Binary W
+                    Binary.setW(1010111);
+                    textView.setText("--> " + Binary.getW());
+                }
+                if (editText.getText().toString().equals("X")) {
+                    //Binary X
+                    Binary.setX(1011000);
+                    textView.setText("--> " + Binary.getX());
+                }
+                if (editText.getText().toString().equals("Y")) {
+                    //Binary Y
+                    Binary.setY(1011001);
+                    textView.setText("--> " + Binary.getY());
+                }
+                if (editText.getText().toString().equals("Z")) {
+                    //Binary Z
+                    Binary.setZ(1011010);
+                    textView.setText("--> " + Binary.getZ());
+                }
+            }
+        });
+
+
+
+
+    }
+}
