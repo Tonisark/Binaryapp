493c12ab5fa025a83bfdfa7a882aa6a2c4feaf66
Tonisark <sogoaheadistill@gmail.com>
2021-09-12 13:55:20 +0430

    New way yo receive Words!

--- app/src/main/java/com/example/binary/MainActivity.java
+++ app/src/main/java/com/example/binary/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 editText = findViewById(R.id.GetBinary);
-                String YY="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+                String YY="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
                 if (YY.contains(editText.getText().toString()))
                 {
                     if (editText.getText().toString().contains("a")) { textView.setText("01100001"); }
